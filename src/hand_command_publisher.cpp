1d4f6acaf62b9e838ad3b2d3616122109fb1dcac
AshayAswale <asaswale@wpi.edu>
2019-04-08 23:57:18 -0400

    Minimal task in callback

--- src/hand_command_publisher.cpp
+++ src/hand_command_publisher.cpp
@@ -4,32 +4,30 @@
 #include <std_msgs/MultiArrayDimension.h>
 
 ros::Publisher* hand_msg_pub;
+std_msgs::Float64MultiArray hand_msg_close, hand_msg_open;
 
+void insertDataToMultiArray()
+{ 
+  for (int i = 0; i < 3; i++)
+    hand_msg_close.data.push_back(2.95);
+  for (int j = 3; j < 6; j++)
+    hand_msg_close.data.push_back(0);
+  for (int k = 6; k < 9; k++)
+    hand_msg_close.data.push_back(0.5);  
+  for (int i = 0; i < 9;i++)
+    hand_msg_open.data.push_back(0);
+}
 
 void callback(ihmc_msgs::HandDesiredConfigurationRosMessage hand_desired_config)
 {
-    std_msgs::Float64MultiArray hand_msg;
-    hand_msg.layout.dim.push_back(std_msgs::MultiArrayDimension());  
-    hand_msg.layout.dim[0].size = 9;
-    hand_msg.layout.dim[0].stride = 1;
-    hand_msg.layout.dim[0].label = "publisher";
-    
   if (hand_desired_config.hand_desired_configuration == 2)
   {
-    for (int i = 0; i < 3; i++)
-      hand_msg.data.push_back(2.95);
-    for (int j = 3; j < 6; j++)
-      hand_msg.data.push_back(0);
-    for (int k = 6; k < 9; k++)
-      hand_msg.data.push_back(0.5);
+  hand_msg_pub->publish(hand_msg_close);
   }
   else if(hand_desired_config.hand_desired_configuration == 1)
   {
-    for (int i = 0; i < 9;i++)
-      hand_msg.data.push_back(0);
+  hand_msg_pub->publish(hand_msg_open);
   }
-
-  hand_msg_pub->publish(hand_msg);
 }
 
 int main(int argc, char** argv)
@@ -39,8 +37,19 @@ int main(int argc, char** argv)
 
   ros::AsyncSpinner spinner(1);
   spinner.start();
+
+  hand_msg_close.layout.dim.push_back(std_msgs::MultiArrayDimension());  
+  hand_msg_close.layout.dim[0].size = 9;
+  hand_msg_close.layout.dim[0].stride = 1;
+  hand_msg_close.layout.dim[0].label = "publisher";
+
+  hand_msg_open = hand_msg_close;   //Same content for both open and close message.
+
+  insertDataToMultiArray();
+
   ros::Publisher publish = nh.advertise<std_msgs::Float64MultiArray>("/reflex_hand_desired_config", 100);
   hand_msg_pub = &publish;
+  // ros::Publisher publish_ihmc = nh.advertise<ihmc_msgs::HandDesiredConfigurationRosMessage>("/ihmc_ros/atlas/control/hand_desired_configuration", 100);
   ros::Subscriber subscribe = nh.subscribe("/ihmc_ros/atlas/control/hand_desired_configuration", 100, callback);
 
   ros::waitForShutdown();
