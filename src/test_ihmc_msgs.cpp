3e71e97e6814df84981625dadf1d160b43a22c04
Ashay Aswale <asaswale@wpi.edu>
2019-10-25 11:42:47 -0400

    changed ihmc_msgs tester to test arm_control_interface

--- src/test_ihmc_msgs.cpp
+++ src/test_ihmc_msgs.cpp
@@ -1,4 +1,5 @@
 #include <ihmc_msgs/ArmDesiredAccelerationsRosMessage.h>
+#include <tough_controller_interface/arm_control_interface.h>
 #include "ros/ros.h"
 
 int main(int argc, char** argv)
@@ -9,20 +10,15 @@ int main(int argc, char** argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  std::string topic = "/ihmc_ros/atlas/control/arm_desired_joint_accelerations";
-  // ros::Publisher acceleration_publisher(topic, 1000, ihmc_msgs::ArmDesiredAccelerationsRosMessage, nh);
-  ros::Publisher acceleration_publisher = nh.advertise<ihmc_msgs::ArmDesiredAccelerationsRosMessage>(topic, 1, true);
-
-  ihmc_msgs::ArmDesiredAccelerationsRosMessage arm_message;
-  arm_message.robot_side = arm_message.LEFT;
-  arm_message.unique_id = rand() % 100;
-  arm_message.desired_joint_accelerations.resize(7);
+  ArmControlInterface arm_controller(nh);
+  std::vector<double> arm_values;
+  arm_values.resize(7);
   for (size_t i = 0; i < 7; i++)
   {
-    arm_message.desired_joint_accelerations.at(i) = 2;
+    arm_values.at(i) = 2;
   }
 
-  acceleration_publisher.publish(arm_message);
+  arm_controller.moveArmJointsAcceleration(RobotSide::LEFT, arm_values);
   ros::Duration(0.5).sleep();
   spinner.stop();
 
